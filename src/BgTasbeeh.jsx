80630c8d61e3a418050509afd97e2d0e909dbec0
Mousamia <87888309+Mousamia@users.noreply.github.com>
2024-03-06 20:56:14 +0600

    subhanallah alhamdulillah

--- src/BgTasbeeh.jsx
+++ src/BgTasbeeh.jsx
@@ -2,20 +2,24 @@ import { useState } from "react";
 
 /* eslint-disable no-unused-vars */
 export default function BgTasbeeh() {
-    const [add, setAdd]  = useState(0);
+    const [add, setAdd] = useState(0);
 
-    const addOne = () =>{     
-      setAdd(add+1);
+    const addOne = () => {
+        setAdd(add + 1);
     }
-    function reset(){
-       const  newNum = 0;
-      setAdd(newNum);
+    function reset() {
+        const newNum = 0;
+        setAdd(newNum);
 
     }
     return (
         <>
+            <h2 className="text-2xl text-blue-500">Say <span>Subhanallah 33 </span></h2>
+            <h2 className="text-2xl text-blue-500">Say <span>Alhamdulillah 33 </span></h2>
+            <h2 className="text-2xl text-blue-500">Say <span>Allahu Akbar 34 </span></h2>
             <div className="w-7/9 h-3/4  mx-auto mt-20">
-                <h3 className="text-9xl mt-5 font-bold"> { add} </h3>
+                <h3 className="text-9xl mt-5 font-bold"> {add} </h3>
+
                 <div>
                     <button onClick={addOne} className="text-2xl bg-sky-500 rounded-md mx-2 px-8 py-4  mt-8">
                         ADD

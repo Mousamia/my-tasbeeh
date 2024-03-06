3ae03a863df6a75dcd446acc8555ef5f366cf508
Mousamia <87888309+Mousamia@users.noreply.github.com>
2024-03-06 15:14:29 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+// eslint-disable-next-line no-unused-vars
+import reactLogo from './assets/react.svg'
+// eslint-disable-next-line no-unused-vars
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // eslint-disable-next-line no-unused-vars
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Tasbeeh</h1>
+        <h3>Utilize Your time Effeciently</h3>
+      </div>
+
+
+
+    </>
+  )
+}
+
+export default App

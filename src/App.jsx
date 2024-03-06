571acf167f07744934e537166f518f5cd702e188
Mousamia <87888309+Mousamia@users.noreply.github.com>
2024-03-06 17:29:54 +0600

    add reset done

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,8 @@ function App() {
     <>
       <div>
         <h1 className='text-7xl font-bold'>Tasbeeh</h1>
-        <h3 className='text-xl font-light'>Utilize Your time Effeciently</h3>
+        <h3 className='text-xl font-light mb-8'>Utilize Your time Effeciently</h3>
+        <img src="./assets/react.svg" alt="" />
       </div>
      <BgTasbeeh></BgTasbeeh>
 
--- src/BgTasbeeh.jsx
+++ src/BgTasbeeh.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
+
 /* eslint-disable no-unused-vars */
 export default function BgTasbeeh() {
+    const [add, setAdd]  = useState(0);
+
+    const addOne = () =>{     
+      setAdd(add+1);
+    }
+    function reset(){
+       const  newNum = 0;
+      setAdd(newNum);
+
+    }
     return (
         <>
             <div className="w-7/9 h-3/4  mx-auto mt-20">
-                <h3 className="text-9xl mt-5 font-bold" id="hisab-field">0</h3>
+                <h3 className="text-9xl mt-5 font-bold"> { add} </h3>
                 <div>
-                    <button className="text-2xl bg-sky-500 rounded-md mx-2 px-8 py-4  mt-8">
+                    <button onClick={addOne} className="text-2xl bg-sky-500 rounded-md mx-2 px-8 py-4  mt-8">
                         ADD
                     </button>
-                    <button className="text-2xl bg-sky-500 rounded-md mx-2 px-8 py-4  my-6">
+                    <button onClick={reset} className="text-2xl bg-sky-500 rounded-md mx-2 px-8 py-4  my-6">
                         RESET
                     </button>
                 </div>

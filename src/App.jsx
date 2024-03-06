f12178d44c989bd59b4c2fcef6a50cae4d718589
Mousamia <87888309+Mousamia@users.noreply.github.com>
2024-03-06 16:20:59 +0600

    desiggggn done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable no-unused-vars */
 import { useState } from 'react'
-// eslint-disable-next-line no-unused-vars
-import reactLogo from './assets/react.svg'
-// eslint-disable-next-line no-unused-vars
-import viteLogo from '/vite.svg'
 import './App.css'
+import BgTasbeeh from './BgTasbeeh.jsx'
+
 
 function App() {
-  // eslint-disable-next-line no-unused-vars
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <h1>Tasbeeh</h1>
-        <h3>Utilize Your time Effeciently</h3>
+        <h1 className='text-7xl font-bold'>Tasbeeh</h1>
+        <h3 className='text-xl font-light'>Utilize Your time Effeciently</h3>
       </div>
-
-
+     <BgTasbeeh></BgTasbeeh>
 
     </>
   )
---
+++ src/BgTasbeeh.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+export default function BgTasbeeh() {
+    return (
+        <>
+            <div className="w-7/9 h-3/4  mx-auto mt-20">
+                <h3 className="text-9xl mt-5 font-bold" id="hisab-field">0</h3>
+                <div>
+                    <button className="text-2xl bg-sky-500 rounded-md mx-2 px-8 py-4  mt-8">
+                        ADD
+                    </button>
+                    <button className="text-2xl bg-sky-500 rounded-md mx-2 px-8 py-4  my-6">
+                        RESET
+                    </button>
+                </div>
+            </div>
+        </>
+    )
+}
